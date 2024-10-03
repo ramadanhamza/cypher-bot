24d54046b9a7f29f31642927ef8682c8b8172b9c
hamzaramadan <sharinganstrength@hotmail.com>
2024-10-03 13:52:42 +0100

    used valorant api to fetch successfully fetch profile data with profile command

--- src/main/java/org/example/commands/Profile.java
+++ src/main/java/org/example/commands/Profile.java
@@ -26,7 +26,42 @@ public class Profile extends ListenerAdapter {
         String name = event.getOption("name").getAsString();
         String tag = event.getOption("tag").getAsString();
 
-        event.reply("Player : " + name + "#" + tag + "\nRegion : " + region.toUpperCase()).queue();
+        String url = "https://api.henrikdev.xyz/valorant/v2/mmr/" + region + "/" + name + "/" + tag;
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("Authorization", Token.VALORANT_API_KEY)
+                .build();
+
+        client.newCall(request).enqueue(new okhttp3.Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                event.reply("Failed to retrieve data from the Valorant API").queue();
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    event.reply("Error: API request failed").queue();
+                    return;
+                }
+
+                String responseBody = response.body().string();
+                JSONObject json = new JSONObject(responseBody);
+
+                JSONObject data = json.getJSONObject("data");
+
+                String playerName = data.getString("name");
+                String playerTag = data.getString("tag");
+                String rank = data.getJSONObject("current_data")
+                        .getString("currenttierpatched");
+
+                event.reply("Player: " + playerName + "#" + playerTag + "\nRegion: " + region + "\nRank: " + rank).queue();
+            }
+        });
 
     }
 }
