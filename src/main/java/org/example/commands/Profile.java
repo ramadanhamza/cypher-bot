7132a15a1cd390661a22adcfa7e04530cbead62c
hamzaramadan <sharinganstrength@hotmail.com>
2024-10-19 19:41:51 +0100

    added pagination test

--- src/main/java/org/example/commands/Profile.java
+++ src/main/java/org/example/commands/Profile.java
@@ -223,7 +223,7 @@ public class Profile extends ListenerAdapter {
             pages.add(Page.of("This is entry Nº " + (i + 1)));
         }
 
-        event.getChannel().sendMessage((MessageCreateData) pages.get(0).getContent()).queue(success -> {
+        event.getChannel().sendMessage("Hello world").queue(success -> {
             Pages.paginate(success, pages, /* Use buttons? */ true);
         });
 
